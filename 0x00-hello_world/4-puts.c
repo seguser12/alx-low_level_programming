4b8e0503dbdec481b2683173fab03031dcbc3580
seguser <professionaleditor6@gmail.com>
2022-09-08 08:19:29 -0700

    add files of all mandatory tasks in c-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	const char *print_stuff;
+
+	print_stuff = "Programming is like building a multilingual puzzle";
+	puts(print_stuff);
+
+	return (0);
+}
