0360c85f5a041b69bb0f0ca336e1eeb94c41e4e7
seguser <professionaleditor6@gmail.com>
2022-09-09 13:25:37 -0700

    update code for advance task 1 (c variables, if, else, while)

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,15 @@ int main(void)
 	{
 		for (num2 = 49; num2 < 58; num2++)
 		{
-			putchar(num1);
-			putchar(num2);
-			if (num1 != 56 || num2 != 57)
+			if (num2 > num1)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(num1);
+				putchar(num2);
+				if (num1 != 56 || num2 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
