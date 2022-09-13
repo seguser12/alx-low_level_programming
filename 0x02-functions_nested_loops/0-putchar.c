f2b67b04eae2a26e4e2aeb6e4d8f53c8e8667f36
seguser <professionaleditor6@gmail.com>
2022-09-13 08:21:28 -0700

    add file for task 0 of c functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	char *to_print = "_putchar";
+	int count = 0;
+	int end = sizeof(to_print) / sizeof(char);
+
+	while (count < end)
+	{
+		_putchar(to_print(count));
+		count++;
+	}
+	_putchar('\n');
+
+	return(0);
+}
