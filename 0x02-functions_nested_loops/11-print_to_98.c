2aee2a9201b927ee609ba9ed07cc158b57953bdd
seguser <professionaleditor6@gmail.com>
2022-09-13 11:27:05 -0700

    update code for task 11 in c function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,31 +8,23 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		_putchar(n + '0');
+		printf("%d\n", n);
 	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
-			_putchar(n + '0');
 			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf("%d, ", n);
 		}
 	}
 	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
-			_putchar(n + '0');
 			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf("%d, ", n);
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
