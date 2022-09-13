669c95b95e87847c24526a68b6c1e2f3800e6dec
seguser <professionaleditor6@gmail.com>
2022-09-13 12:01:25 -0700

    update code to compile

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d", n)
+			printf("%d", n);
 			if (n != 98)
 				printf(", ");
 		}
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d", n)
+			printf("%d", n);
 			if (n != 98)
 				printf(", ");
 		}
