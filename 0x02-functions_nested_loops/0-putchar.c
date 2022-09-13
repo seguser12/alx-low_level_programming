df2d8eb8252f48c5c7d6f3cda7b2fdf66bbb6ad5
seguser <professionaleditor6@gmail.com>
2022-09-13 08:29:25 -0700

    update file for task 0 c nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *to_print = "_putchar";
 	int count = 0;
-	int end = sizeof(to_print) / sizeof(char);
+	int end = sizeof(*to_print) / sizeof(char);
 
 	while (count < end)
 	{
@@ -18,5 +18,5 @@ int main(void)
 	}
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
