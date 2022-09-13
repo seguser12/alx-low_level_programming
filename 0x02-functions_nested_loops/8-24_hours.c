e390ba1bc63867b4cf0e0650da958442e02e3280
seguser <professionaleditor6@gmail.com>
2022-09-13 13:31:18 -0700

    refactor code of task 8 c functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,21 +12,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			if (hour < 10)
-			{
-				_putchar('0');
-				_putchar(hour + '0');
-				_putchar(':');
-			}
-			_putchar(hour + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-
-			if (min < 10)
-			{
-				_putchar('0');
-				_putchar(min + '0');
-			}
-			_putchar(min + '0');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
