db49da95e01c15de552b86e4897fbd7f368f941f
seguser <professionaleditor6@gmail.com>
2022-09-30 09:16:32 -0700

    write complete code of 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,4 +5,16 @@
  * @s1: first string
  * @s2: second string
  *
- * Return:
+ * Return: compared string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int n, diff;
+
+	for (i = 0; s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+	}
+	diff = s1[i] - s2[i];
+}
