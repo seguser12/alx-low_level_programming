45d25021ccc3376df800a766341b7cca8d46c624
seguser <professionaleditor6@gmail.com>
2022-09-30 09:31:10 -0700

    add return statement to code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, diff;
 
-	for (i = 0; s1[i] == s2[i])
+	for (i = 0; s1[i] == s2[i]; i++)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
 	}
 	diff = s1[i] - s2[i];
+
+	return (diff);
 }
