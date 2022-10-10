5422f5e5792ca4f74de6343ef02a05417a0528a4
seguser <gbodisegun@gmail.com>
2022-10-10 16:34:37 -0700

    update code of task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (!d)
-	{
 		d = malloc(sizeof(struct dog));
-		if (!d)
-			return (NULL);
-	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
